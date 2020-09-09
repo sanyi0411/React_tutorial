61b67c370ba26d6b8f386e1ab94a5ad388580da0
sanyi0411 <sandor.derda@gmail.com>
2020-09-09 17:19:30 +0200

    Separate files

---
+++ 5-separate_files/src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Sandor DERDA</h1>        
+            <p>I am who I am</p>
+            <ul>
+                <li>Iceland</li>
+                <li>Japan</li>
+                <li>Cuba</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
+// TODO: learn more about exports in ES6
---
+++ 5-separate_files/src/index.js
@@ -0,0 +1,11 @@
+// Use the code from previous task but create a separate file
+// for the functional component and import it in index.js 
+
+import React from "react"
+import ReactDOM from "react-dom"
+import MyInfo from "./components/MyInfo"
+
+ReactDOM.render(
+    <MyInfo />,
+    document.getElementById("root")
+)
