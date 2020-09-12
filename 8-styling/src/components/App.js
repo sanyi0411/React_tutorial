df99486a070794aed5d56c1bb79e82cf1ec510ed
sanyi0411 <sandor.derda@gmail.com>
2020-09-12 10:15:24 +0200

    Styling

---
+++ 8-styling/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./Header"
+import MainComponent from "./MainComponent"
+import Footer from "./Footer"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainComponent />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ 8-styling/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className="navbar">
+            <nav>
+                This is the navbar
+            </nav>
+        </header>
+    )
+}
+
+export default Header
