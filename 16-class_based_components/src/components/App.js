5d649faa441bd1686196079b09b98ed8dceb8b0a
sanyi0411 <sandor.derda@gmail.com>
2020-09-12 18:05:42 +0200

    Class based components

---
+++ 16-class_based_components/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class App extends React.Component {
+
+    yourMethodHere() {
+
+    }
+
+    render() {
+        this.yourMethodHere();
+        return (
+            <div>
+                <h1>Hello world</h1>
+            </div>
+        )
+    }
+}
+
+export default App
