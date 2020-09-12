8b7c082d5c53a64ac9623d05f76dc9930c08cda1
sanyi0411 <sandor.derda@gmail.com>
2020-09-12 09:55:28 +0200

    Todo app (phase 1)

---
+++ 7-todo_app_phase_1/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox"></input>
+            <p>Placeholder text</p>
+            <input type="checkbox"></input>
+            <p>Placeholder text</p>
+            <input type="checkbox"></input>
+            <p>Placeholder text</p>
+        </div>
+    )
+}
+
+export default App
---
+++ 7-todo_app_phase_1/src/index.js
@@ -0,0 +1,14 @@
+// Initialize a React app from scratch
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you are making a todo list with some hard-coded items on it
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App"
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
